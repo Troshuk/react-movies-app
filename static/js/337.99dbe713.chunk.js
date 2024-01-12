b4a022aee23eb389673f757a0ffbc2c21f486fea
Troshuk <Troshuk@gmail.com>
2024-01-12 21:09:03 +0000

    Deploying to gh-pages from @ Troshuk/react-movies-app@24ba41fdbda6ea65ea34e35682a25d5e42b21856 🚀

---
+++ static/js/337.99dbe713.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_movies_app=self.webpackChunkreact_movies_app||[]).push([[337],{75:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var r=t(975),s=t(212),a="TrandingList_title__vOrff",i=t(184),c=function(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)("h1",{className:a,children:"Tranding today"}),(0,i.jsx)(s.a,{getMovies:r.Df})]})},u=function(){return(0,i.jsx)(c,{})}}}]);
+//# sourceMappingURL=337.99dbe713.chunk.js.map
